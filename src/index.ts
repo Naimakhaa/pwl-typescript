14d3932d5e2f6ca5cfe5938ef6ef27d7458c11b5
Naimakhaa <h1101241050@student.untan.ac.id>
2026-02-25 04:43:47 +0000

    commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function hello(nama: String): string{
+    return `Selamat Datang `+nama+ ` di PWL, umur `+nama+ ` adalah `+umur+``;
+}
+const mahasiswa ="Kharizma";
+const umur: number = 15;
+console.log(hello(mahasiswa));
